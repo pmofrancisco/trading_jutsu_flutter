890def4e3288ce8fa5c1e206fc67ae5d06311f95
Paul Michael Francisco <pmofrancisco@yahoo.com>
2024-06-03 20:46:24 +0800

    Created app.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:trading_jutsu_flutter/main.dart';
+import 'package:trading_jutsu_flutter/src/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const App());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
